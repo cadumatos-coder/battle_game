19953a6014fc60851699b21e8c94c6eafa73bb52
Carlos Matos <matoscadu14@gmail.com>
2025-10-19 11:21:19 -0700

    Added player and opponent containers
    Replaced player and opponent variables with their respective containers.

--- battle_game.py
+++ battle_game.py
@@ -31,16 +31,24 @@ def main():
     max_damage = 50
     least_damage = 5
     
-    # Player settings
-    user_health = 100
-    user_damage = 0
-    user_shield = 60
+    player_container = [
+        100, # initial user health
+        0,   # initial user damage
+        60,  # initial user shield health
+        0,   # default user decision
+        0,   # default user weapon
+        0    # default user defense
+    ]
     
     
-    # Opponent settings
-    op_damage = random.randint (least_damage, max_damage)
-    op_shield_health = 60
-    op_health = 100
+    opponent_container = [
+        100, # initial opponent health
+        0,   # initial opponent damage
+        60,  # initial opponent shield health
+        0,   # default opponent decision
+        0,   # default opponent weapon
+        0    # default opponent defense
+    ]
     
     # Decide if player starts
     player_starts = random.choice([True, False])
@@ -60,148 +68,186 @@ def main():
     
         # Round settings
         dodge_percent = random.randint (0, 20)
-        damage = random.randint (least_damage, max_damage)
+        player_container[1] = random.randint (least_damage, max_damage) # player_container[1] is user_damage
+        opponent_container [1] = random.randint (least_damage, max_damage)
     
         # Player decision making
-        player_decision = getIntInput("Do you want to (1) attack or (2) defend? ",[1,2])
-        if player_decision == 1: # attack
-            player_weapon = getIntInput("Do you want to use (1) the hammer, (2) the bow and arrow, or (3) the sword? ",[1,2,3])
-        elif player_decision == 2: # defend
-            if user_shield <= 0:
-                player_defense = 2 # can only dodge since shield is broken
+      # player_container[3] is player_decision
+        player_container[3] = getIntInput("Do you want to (1) attack or (2) defend? ",[1,2])
+        if player_container[3] == 1: # attack
+          # player_container[4] is player_weapon
+            player_container[4] = getIntInput("Do you want to use (1) the hammer, (2) the bow and arrow, or (3) the sword? ",[1,2,3])
+        elif player_container[3] == 2: # defend
+            if player_container[2] <= 0: # player_container[2] is player_shield_health
+              # player_container[5] is player_defense
+                player_container[5] = 2 # can only dodge since shield is broken
                 print ("Your shield is broken, you can only try to dodge!")        
             else:
-                player_defense = getIntInput("Do you want to (1) block with your shield, or (2) try to dodge the attack? ",[1,2])
+                player_container[5] = getIntInput("Do you want to (1) block with your shield, or (2) try to dodge the attack? ",[1,2])
     
         # Opponent decision making
-        opponent_decision = random.choice([1, 2]) # 1: attack, 2: defend
-        print(f"Your opponent's decision: {opponent_decision}")
-        if opponent_decision == 1: # attack
-            opponent_weapon = random.choice([1, 2, 3]) # 1: hammer, 2: bow and arrow, 3: sword
-            print(f"Your opponent's weapon: {opponent_weapon}")
-        elif opponent_decision == 2: # defend
-            if op_shield_health <= 0: # shield is broken
-                opponent_defense = 2 # The opponent can only dodge, as their shield is broken
+        opponent_container [3] = random.choice([1, 2]) # 1: attack, 2: defend
+        print(f"Your opponent's decision: {opponent_container [3]}") # opponent_container[3] is opponent_decision
+        if opponent_container [3] == 1: # attack
+            # opponent_container[4] is opponent_weapon
+            opponent_container [4] = random.choice([1, 2, 3]) # 1: hammer, 2: bow and arrow, 3: sword
+            print(f"Your opponent's weapon: {opponent_container [4]}")
+            # opponent_container[3] is opponent_decision
+        elif opponent_container [3] == 2: # defend
+            # opponent_container[2] is opponent_shield_health
+            if opponent_container[2] <= 0: # shield is broken
+              # opponent_container[5] is opponent_defense
+                opponent_container[5] = 2 # The opponent can only dodge, as their shield is broken
             else:
-                opponent_defense = random.choice([1, 2]) # 1: block with shield, 2: try to dodge attack
-            print(f"Your opponent's defense: {opponent_defense}")
+                opponent_container [5] = random.choice([1, 2]) # 1: block with shield, 2: try to dodge attack
+            print(f"Your opponent's defense: {opponent_container [5]}")
             
         print()
         
     
         # If player goes first
         if player_starts:
-            if player_decision == 1: # if player chooses to attack
-                if opponent_decision == 2: # if the opponent chooses to defend
-                    if opponent_defense == 1: # if the opponent blocks the attack with their shield
+            # player_container[3] is player_decision
+            if player_container [3] == 1: # if player chooses to attack
+                if opponent_container [3] == 2: # if the opponent chooses to defend
+                     # opponent_container[5] is opponent_defense
+                    if opponent_container [5] == 1: # if the opponent blocks the attack with their shield
                         print ("Your opponent has blocked your attack with their shield!")
-                        op_shield_health -= damage
-                        print (f"Your opponent's shield's durability has gone down to {op_shield_health}")
-                    elif opponent_defense == 2: # if the opponent tries to dodge attack
+                       # opponent_container[2] is opponent_shield_health
+                        opponent_container [2] -= player_container [1] # player_container[1] is user_damage
+                        print (f"Your opponent's shield's durability has gone down to {opponent_container [2]}")
+                    elif opponent_container [5] == 2: # if the opponent tries to dodge attack
                         print("Your opponent has tried to dodge the attack!")
                         if dodge_percent >= 10:
                             print ("Your opponent has dodged your attack!")
                         elif dodge_percent < 10:
                             print("You did damage!")
-                            op_health -= damage
-                            print (f"Your opponent's health has gone down to {op_health}")    
-                elif opponent_decision == 1: # if the opponent chooses to attack
+                          # opponent_container[0] is opponent_health
+                            opponent_container [0] -= player_container [1]
+                            # player_container[1] is user_damage
+                            print (f"Your opponent's health has gone down to {opponent_container [0]}")    
+                   # opponent_container[3] is opponent_decision
+                elif opponent_container [3] == 1: # if the opponent chooses to attack
                     print ("You both attacked each other! You both received damage!")
-                    op_health -= damage
-                    user_health -= damage
-                    print (f"Your health is now at {user_health}")
-                    print (f"Your opponent's health is now at {op_health}")
-                    if op_health <= 0:
+                  # opponent_container[0] is opponent_health
+                    opponent_container [0] -= player_container [1] # player_container[1] is player_damage
+                    # player_container[1] is user_damage
+                    player_container [0] -= opponent_container [1]
+                    # player_container[0] is user_health
+                    print (f"Your health is now at {player_container [0]}")
+                    print (f"Your opponent's health is now at {opponent_container [0]}")
+                    if opponent_container [0] <= 0:
                         print ("You have defeated your opponent!")
                         print ("Congrats!")
                         break
-                    elif user_health <= 0:
+                    elif player_container [0] <= 0:
                         print ("You have been defeated!")
                         print ("Sorry!")
                         break
-            elif player_decision == 2: # player chooses to defend
-                if opponent_decision == 1: # opponent chooses to attack
-                    if player_defense == 1: # player uses shield
+                    # player_container[0] is user_health
+            elif player_container [3] == 2: # player chooses to defend
+            # player_container[3] is player_decision
+                if opponent_container [3] == 1: # opponent chooses to attack
+                    if player_container [5] == 1: # player uses shield
+                        # player_container[5] is user_defense
                         print ("You blocked your opponent's attack!")
-                        user_shield -= damage
-                        print (f"You shield's durability has now gone down to {user_shield}")
+                        player_container [2] -= opponent_container [1]
+                        print (f"You shield's durability has now gone down to {player_container[2]}")
+                        # player_container[2] is player_shield_health
     
-                    elif player_defense == 2: # player tries to dodge opponent attack
+                    elif player_container [5] == 2: # player tries to dodge opponent attack
+                        # player_container[5] is user_defense
                         if dodge_percent >= 10: # if random number is equal to or greater than 10 
                             print ("You were able to dodge the attack!")       
                         elif dodge_percent < 10: # if random number is less than 10
                             print ("You weren't able to dodge the attack!")
-                            user_health -= damage
-                            print (f"Your health is now at {user_health}")
+                            player_container [0] -= opponent_container [1]
+                            print (f"Your health is now at {player_container [0]}")
+                            # player_container[0] is player_health
                             
-                elif opponent_decision == 2: # opponent also defends
+                elif opponent_container [3] == 2: # opponent also defends
                     print ("You both defended!")
                     print ("No one has taken damage!")
             
         # If opponent goes first
         if player_starts == False:
-            if opponent_decision == 1: # the opponent chooses to attack
-                if player_decision == 1: # player also chooses to attack
+            if opponent_container [3] == 1: # the opponent chooses to attack
+                if player_container [3] == 1: # player also chooses to attack
                     print ("You both attacked each other!")
-                    user_health -= damage
-                    op_health -= damage
-                    print (f"Your health is now at {user_health}")
-                    print (f"Your opponent's health is now at {op_health}")
-                    if user_health <= 0: # health goes down to 0 and player loses
+                    # player_container[3] is player_decision
+                    player_container [0] -= opponent_container [1]
+                    # player_container[0] is player_health
+                    opponent_container [0] -= player_container [1]
+                    # player_container[1] is user_damage
+                    print (f"Your health is now at {player_container [0]}")
+                    print (f"Your opponent's health is now at {opponent_container [0]}")
+                    if player_container [0] <= 0: # health goes down to 0 and player loses
                         print ("Sorry! You've been defeated by your opponent!")
                         break
-                    elif op_health <= 0: # opponent health goes down to 0 and player wins
+                    # player_container[0] is user_health
+                    elif opponent_container [0] <= 0: # opponent health goes down to 0 and player wins
                         print ("Hooray! You've defeated your opponent!")
                         print ("Congratulations!")
                         break
-                if player_decision == 2: # player chooses to defend
-                    if player_defense == 1: # chooses to block with shield
+                if player_container [3] == 2: # player chooses to defend
+                    # player_container[3] is player_decision
+                    if player_container [5] == 1: # chooses to block with shield
                         print ("You blocked your opponent's attack!")
-                        user_shield -= damage
-                        print (f"Your shield durability has gone down to {user_shield}")
+                        # player_container[5] is user_defense
+                        player_container [2] -= opponent_container [1]
+                        print (f"Your shield durability has gone down to {player_container [2]}")
+                        # player_container[2] is player_shield_health
                     
-                    elif player_defense == 2: # player tries to dodge the attack
+                    elif player_container [5] == 2: # player tries to dodge the attack
+                        # player_container[5] is user_defense
                         if dodge_percent >= 10: # random number is equal to or greater than 10 
                             print ("You were successful in dodging the attack!")
                         elif dodge_percent < 10:
                             print ("You took damage! You couldn't dodge the attack! Sorry!")
-                            user_health -= damage
-                            print (f"Your health is now at {user_health}")
+                            player_container [0] -= opponent_container [1]
+                            # player_container[0] is player_health
+                            print (f"Your health is now at {player_container [0]}")
             
-            elif opponent_decision == 2: # opponent defends
-                if player_decision == 2: # player defends
+            elif opponent_container [3] == 2: # opponent defends
+                if player_container [3] == 2: # player defends
+                    # player_container[3] is player_decision
                     print ("You both defended!")
                     print ("No one took damage!")
-                elif player_decision == 1: # player chooses to attack
-                    if opponent_defense == 1: # opponent blocks with shield
+                elif player_container [3] == 1: # player chooses to attack
+                    # player_container[3] is player_decision
+                    if opponent_container [5] == 1: # opponent blocks with shield
                         print ("Your opponent blocked your attack with their shield!")
-                        op_shield_health -= damage
-                        print (f"Your opponent's shield's durability has gone down to {op_shield_health}")
-                    elif opponent_defense == 2: # opponent tries to dodge attack
+                        opponent_container [2] -= player_container [1]
+                        # player_container[1] is user_damage
+                        print (f"Your opponent's shield's durability has gone down to {opponent_container [2]}")
+                    elif opponent_container [5] == 2: # opponent tries to dodge attack
                         if dodge_percent >= 10: # random number is greater than or equal to 10
                             print ("Your opponent has dodged your attack!")
                         elif dodge_percent < 10: # random number is less than 10
                             print ("You did damage, your opponent tried to dodge the attack, but was unsuccessful!")
-                            op_health -= damage
-                            print (f"Your opponent's health is now at {op_health}")
+                            opponent_container [0] -= player_container [1]
+                            # player_container[1] is user_damage
+                            print (f"Your opponent's health is now at {opponent_container [0]}")
     
         # Next round definitions
         # (Does the player's shield have enough health?)
-        if user_shield <= 0:
+        if player_container [2] <= 0:
+            # player_container[2] is player_shield_health
             print ("Your shield has broken and cannot be used again!")
     
     
         # (Does the player have enough health?)
-        if user_health <= 0:
+        if player_container [0] <= 0:
+            # player_container[0] is user_health
             print ("Sorry! You've died, your health reached 0!")
             break
     
         # (Does the player's shield have enough health?)
-        if op_shield_health <= 0:
+        if opponent_container [2] <= 0:
             print ("Your opponent's shield is broken!")
     
         # (Does the player have enough health?)
-        if op_health <= 0:
+        if opponent_container [0] <= 0:
             print ("You've won! You've defeated your opponent!")
             break
 
